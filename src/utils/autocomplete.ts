ada0666e8b2f29ee5d59708632b1a1162c1a53ec
Not <hey@toastify.tk>
2023-05-24 12:40:26 +0000

    fix autocomplete errors (by ignoring them)

--- src/utils/autocomplete.ts
+++ src/utils/autocomplete.ts
@@ -5,19 +5,23 @@ import { abbreviate } from "./abbreviate.js";
 export async function channelAutocomplete(
   interaction: AutocompleteInteraction
 ) {
-  const focusedValue = interaction.options.getFocused() || "mrbeast";
-  const { data } = await axios.get<
-    any,
-    AxiosResponse<{
-      results: { name: string; id: string; mainCount: number }[];
-    }>
-  >(
-    `https://livecounts.xyz/api/youtube-live-subscriber-count/search/${focusedValue}`
-  );
-  await interaction.respond(
-    data.results.map((channel) => ({
-      name: `${channel.name} • ${abbreviate(channel.mainCount)} subscribers`,
-      value: channel.id,
-    }))
-  );
+  try {
+    const focusedValue = interaction.options.getFocused() || "mrbeast";
+    const { data } = await axios.get<
+      any,
+      AxiosResponse<{
+        results: { name: string; id: string; mainCount: number }[];
+      }>
+    >(
+      `https://livecounts.xyz/api/youtube-live-subscriber-count/search/${focusedValue}`
+    );
+    await interaction.respond(
+      data.results.map((channel) => ({
+        name: `${channel.name} • ${abbreviate(channel.mainCount)} subscribers`,
+        value: channel.id,
+      }))
+    );
+  } catch (error) {
+    // do nothing
+  }
 }
